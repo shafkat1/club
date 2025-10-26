c82a4eba7915b93049721984fe2419182bbf0377
Club Dev <dev@desh.co>
2025-10-25 23:25:41 -0400

    Fix ACM set indexing by converting to list with tolist()

--- infra/terraform/acm.tf
+++ infra/terraform/acm.tf
@@ -22,11 +22,11 @@ resource "aws_acm_certificate" "alb" {
 
 # DNS validation records in Route 53 (only if enable_domain=true)
 resource "aws_route53_record" "cloudfront_cert_validation" {
-  count   = var.enable_domain ? length(aws_acm_certificate.cloudfront.domain_validation_options) : 0
-  name    = aws_acm_certificate.cloudfront.domain_validation_options[count.index].resource_record_name
-  type    = aws_acm_certificate.cloudfront.domain_validation_options[count.index].resource_record_type
+  count   = var.enable_domain ? length(tolist(aws_acm_certificate.cloudfront.domain_validation_options)) : 0
+  name    = tolist(aws_acm_certificate.cloudfront.domain_validation_options)[count.index].resource_record_name
+  type    = tolist(aws_acm_certificate.cloudfront.domain_validation_options)[count.index].resource_record_type
   zone_id = aws_route53_zone.primary[0].zone_id
-  records = [aws_acm_certificate.cloudfront.domain_validation_options[count.index].resource_record_value]
+  records = [tolist(aws_acm_certificate.cloudfront.domain_validation_options)[count.index].resource_record_value]
   ttl     = 60
 }
 
@@ -38,11 +38,11 @@ resource "aws_acm_certificate_validation" "cloudfront" {
 }
 
 resource "aws_route53_record" "alb_cert_validation" {
-  count   = var.enable_domain ? length(aws_acm_certificate.alb.domain_validation_options) : 0
-  name    = aws_acm_certificate.alb.domain_validation_options[count.index].resource_record_name
-  type    = aws_acm_certificate.alb.domain_validation_options[count.index].resource_record_type
+  count   = var.enable_domain ? length(tolist(aws_acm_certificate.alb.domain_validation_options)) : 0
+  name    = tolist(aws_acm_certificate.alb.domain_validation_options)[count.index].resource_record_name
+  type    = tolist(aws_acm_certificate.alb.domain_validation_options)[count.index].resource_record_type
   zone_id = aws_route53_zone.primary[0].zone_id
-  records = [aws_acm_certificate.alb.domain_validation_options[count.index].resource_record_value]
+  records = [tolist(aws_acm_certificate.alb.domain_validation_options)[count.index].resource_record_value]
   ttl     = 60
 }
 
